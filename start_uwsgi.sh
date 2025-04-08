2a7803442bfec040ce855fe4efba605c930f2f22
Dmitriy Sinitskiy <dmitriy.sinitskiy@quantori.com>
2025-04-08 20:09:23 +0400

    add files

--- start_uwsgi.sh
+++ start_uwsgi.sh
@@ -6,4 +6,4 @@ uwsgi \
   --chdir=./src \
   --module=social.wsgi:application \
   --master --pidfile=/tmp/project-master.pid \
-  --http=0.0.0.0:8000 --processes=5 --uid=1000 --gid=1000 --harakiri=20 --max-requests=5000 --vacuum
+  --http=0.0.0.0:8000 --processes=6 --uid=1000 --gid=1000 --harakiri=20 --max-requests=5000 --vacuum
