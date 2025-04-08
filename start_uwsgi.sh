3f8f3687ce3382fdd3eae2418bc311ab6df9e2d7
Dmitriy Sinitskiy <dmitriy.sinitskiy@quantori.com>
2025-04-08 17:52:07 +0400

    add uwsgi

--- start_uwsgi.sh
+++ start_uwsgi.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
 python src/manage.py create_tables
-#uwsgi \
-#  --chdir=/app \
-#  --module=qblog.wsgi:application \
-#  --env DJANGO_SETTINGS_MODULE=qblog.settings.server_dev \
-#  --master --pidfile=/tmp/project-master.pid \
-#  --http=0.0.0.0:8000 --processes=5 --uid=1000 --gid=1000 --harakiri=20 --max-requests=5000 --vacuum
 
 uwsgi \
   --chdir=./src \
